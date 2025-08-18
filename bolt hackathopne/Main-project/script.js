3e3087baa58a1a9e30b2af9bd416e923fcc3a5a6
Aftab Alam <course.aftabalam@gmail.com>
2025-08-18 08:24:49 +0530

    Delete filemanager diectory

--- bolt hackathopne/Main-project/script.js
+++
@@ -1,7 +0,0 @@
-const startBtn = document.querySelector("#start-btn");
-startBtn.addEventListener('click', function(){
-    const startPage = document.querySelector("#start-page");
-    const lockScreenPage = document.querySelector("#lock-screen-page");
-    startPage.style.display = 'none';
-    lockScreenPage.style.display = 'block';
-})
